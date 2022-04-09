fa96b018dccf1990262a560d77705e4a40d4551a
henrynoble01 <henrynoble01@gmail.com>
2022-04-09 14:43:38 +0100

    chore: adding preboot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { TransferHttpCacheModule } from '@nguniversal/common';
 
+import { PrebootModule } from 'preboot';
+
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { PanelComponent } from './panel/panel.component';
@@ -21,6 +23,7 @@ import { OrganizationsComponent } from './organizations/organizations.component'
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
     TransferHttpCacheModule,
+    PrebootModule.withConfig({ appRoot: 'app-root' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
